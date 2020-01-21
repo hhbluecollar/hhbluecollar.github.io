bc97ad272f8d2bd152a7956a3a077ea6785f473d
hhhbluecollar <henokhamza@gmail.com>
2020-01-20 23:20:08 -0600

    Week 2 day 1

---
+++ scripts/ascii.js
@@ -0,0 +1,83 @@
+
+"use strict";
+
+let start, stop, animation, size, speed, textarea;
+let animationValue = "Blank", delay = 250, interval, fontsize = "7pt";
+let splitText = "=====\n", currentPosition = 0;
+
+window.onload = function () {
+
+    start = document.getElementById('start');
+    stop = document.getElementById('stop');
+    animation = document.getElementById('animation');
+    size = document.getElementById('fontsize');
+    speed = document.getElementById('turbo');
+    textarea = document.getElementById('text-area');
+    /*
+        load clickable(s)
+    */
+    start.onclick = startAnimation;
+    stop.onclick = stopAnimation;
+    animation.onclick = animationSelect;
+    size.onclick = sizeSelect;
+    speed.onclick = speedChange;
+}
+
+function startAnimation() {
+    console.log("STARTED");
+    textarea.value = "";
+    start.disabled = true;
+    stop.disabled = false;
+    animation.disabled = true;
+    animationProgress();
+}
+
+function animationProgress(){
+    animationValue = animation.options[animation.selectedIndex].value;
+    let data = ANIMATIONS[animationValue].split(splitText);
+
+    interval = setInterval(function () {
+    fontsize = size.options[size.selectedIndex].value;
+    textarea.style.fontSize = fontsize;
+
+        if (currentPosition === data.length) {
+            textarea.value = "";
+            currentPosition = 0;
+        }
+        textarea.value = data[currentPosition];
+        currentPosition++;
+    }, delay);
+}
+
+function stopAnimation() {
+
+    stop.disabled = true;
+    start.disabled = false;
+    animation.disabled = false;
+    currentPosition = 0;
+    clearInterval(interval);
+}
+
+function animationSelect() {
+    animationValue = animation.options[animation.selectedIndex].value;
+    textarea.value = ANIMATIONS[animationValue];
+}
+
+function sizeSelect() {
+    fontsize = size.options[size.selectedIndex].value;
+    textarea.style.fontSize = fontsize;
+}
+
+function speedChange() {
+    if (speed.checked) {
+        delay = 50; 
+       console.log("SPEED Faster! delay of " + delay);
+    } 
+    else {
+        delay = 250;
+        console.log("SPEED slower! delay of " + delay);        
+    }
+
+    clearInterval(interval);
+    animationProgress();
+}
