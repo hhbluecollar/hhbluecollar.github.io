d496553da84d02a70252bcfc3ef2e024af4eb724
hhhbluecollar <henokhamza@gmail.com>
2020-01-28 23:23:24 -0600

    Week 3 Day 2

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,7 +1,12 @@
   
  $(function(){
-     
+        "use strict";
+
         let startClicked  = false;
+        /*
+        * change the border color and display the message
+        * check only if the game is stared only
+        */
         $("div.boundary").mouseenter(function(){
             if(startClicked){
                     $("div.boundary").css("background-color","#ff8080");
@@ -11,6 +16,10 @@
             }
             });
 
+        /*
+        * starts the game 
+        * 
+        */
         $("#start").click(function(){
             startClicked = true;
             $("div.boundary").removeAttr('style');
@@ -26,7 +35,11 @@
             //             $("#status" ).text("You lose!"); 
             //             $("#status" ).text('Click the "S" to begin.');
             //     }
-
+        /*
+        * change the border color and display the message when a player leaves the border
+        * set for the game.
+        * check only if the game is stared only
+        */
             $("#maze").mouseleave(function(){
                 if(startClicked){
                     $("div.boundary").css("background-color","#ff8080");
@@ -34,7 +47,10 @@
                     startClicked = false;
                 }
             });         
-
+        /*
+        * Once the game is started if the end is clicked then show message.
+        * check only if the game is stared only
+        */
             $("div#end").click(function(){
                 if(startClicked){
                     $("#status" ).text('Congrats! You won!');   
